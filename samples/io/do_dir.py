031bdce28e5873c6c68378ab85fa3ef81a35ea2e
di6hao <di6hao@126.com>
2021-04-16 13:23:53 +0800

    test

--- samples/io/do_dir.py
+++ samples/io/do_dir.py
@@ -13,6 +13,7 @@ for f in os.listdir(pwd):
     fsize = os.path.getsize(f)
     ftime = os.path.getatime(f)
     print(ftime)
-    mtime = datetime.fromtimestamp(os.path.getmtime(f)).strftime('%Y-%m-%d %H:%M')
+    print('hello')
+    time = datetime.fromtimestamp(os.path.getmtime(f)).strftime('%Y-%m-%d %H:%M')
     flag = '/' if os.path.isdir(f) else ''
     print('%10d  %s  %s%s' % (fsize, mtime, f, flag))
